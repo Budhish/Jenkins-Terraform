0daa1e4468e3f2af8be7422713ced242a89e5ee4
budhish <budhish1312@gmail.com>
2026-06-11 14:14:47 +0530

    adding jenkins.sh

--- modules/ec2/jenkins.sh
+++ modules/ec2/jenkins.sh
@@ -16,6 +16,17 @@ rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
 # Install Java
 yum install java-21-amazon-corretto -y
 
+#Install terraform 
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+#Install Kubectl 
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.3/2026-04-08/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+kubectl version --client
+
 # Install Jenkins
 yum install jenkins -y
 
